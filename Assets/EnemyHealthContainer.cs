8e7923b01035e6597ad9265b3407b28cfe86a658
Charlie <764189@newcollege.ac.uk>
2025-02-14 11:45:12 +0000

    ui?

---
+++ Assets/EnemyHealthContainer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityUI;
+
+public class EnemyHealthContainer : MonoBehaviour
+{
+    [SerializeField] private ImageConversion fillAmountImage;
+    public ImageConversion FillAmountImage => fillAmountImage;
+}
