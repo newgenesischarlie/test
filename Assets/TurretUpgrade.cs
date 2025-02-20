30847975e80eda55a4a911274a14f80651b0f4a1
Charlie <764189@newcollege.ac.uk>
2025-02-20 18:53:37 +0000

    s

--- Assets/TurretUpgrade.cs
+++ Assets/TurretUpgrade.cs
@@ -14,54 +14,62 @@ public class TurretUpgrade : MonoBehaviour
     [Range(0, 1)]
     [SerializeField] private float sellPert;
 
+    // Public properties for accessing values externally, if needed
     public float SellPerc { get; private set; }
     public int UpgradeCost { get; private set; }
     public int Level { get; private set; }
 
     private TurretProjectile _turretProjectile;
 
+    // Unity Start method (should be capitalized)
     private void Start()
     {
         _turretProjectile = GetComponent<TurretProjectile>();
 
+        // Initialize upgrade cost, sell percentage, and level
         UpgradeCost = upgradeInitialCost;
         SellPerc = sellPert;
         Level = 1;
     }
 
-    // Upgrade the turret
-    public void UpgradeTurret()
-    {
-        if (CurrencySystem.Instance.TotalCoins >= UpgradeCost)
+    // Method to upgrade the turret if the player has enough coins
+       public void UpgradeTurret()
+     {
+        // Check if the player has enough coins
+       if (CurrencySystem.Instance.TotalCoins >= UpgradeCost)
         {
-            _turretProjectile.Damage += damageIncremental;
+            // Upgrade turret attributes
+           _turretProjectile.Damage += damageIncremental;
             _turretProjectile.DelayPerShot -= delayReduce;
 
-            UpdateUpgrade();
+            // Update cost and level after upgrading
+           UpdateUpgrade();
         }
-        else
+       else
         {
-            Debug.Log("Not enough coins to upgrade!");
+            // Optionally, you could add some feedback here, e.g., a sound or message to indicate insufficient coins
+           Debug.Log("Not enough coins to upgrade!");
         }
     }
 
+    // Get the sell value for the turret (how much player gets back)
     public int GetSellValue()
     {
         int sellValue = Mathf.RoundToInt(UpgradeCost * SellPerc);
         return sellValue;
     }
 
+    // Private method to handle the upgrade costs and level increment
     private void UpdateUpgrade()
     {
-        // Remove coins for the upgrade
-        CurrencySystem.Instance.RemoveCoins(UpgradeCost);
+        // Remove the coins for the upgrade
+       CurrencySystem.Instance.RemoveCoins(UpgradeCost);
 
-        // Increment the upgrade cost
+        // Increment the upgrade cost for the next upgrade
         UpgradeCost += upgradeCostIncremental;
 
-        // Increase turret level
+        // Increase the turret's upgrade level
         Level++;
     }
 }
-
 
